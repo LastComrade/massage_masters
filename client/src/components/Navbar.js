262e8e74b31e010de1b6ac7a628bb3146dd076d3
1915301 <66868588+1915301@users.noreply.github.com>
2023-03-19 23:02:56 +0530

    Updated navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,39 +2,36 @@
 function Navbar() {
     return (
         // <h1>hello world</h1>
-        <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg bg-light ">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="/">Massage</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-end">
+                <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">Home</a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">Book Appointmeny</a>
                 </li>
-                <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><hr class="dropdown-divider"/></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
+                <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">Services</a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
+                <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">About Us</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">Gallery</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">Testimonials</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">Contact Us</a>
                 </li>
             </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
             </div>
         </div>
         </nav>
